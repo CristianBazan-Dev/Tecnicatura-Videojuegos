4726258912c6136365c1b7a873b7bd0ddb708a45
Cristian Bazan <cristian.bazanbandini@rcpo.com.ar>
2024-09-15 23:13:16 -0300

    Ejercicio 3: Ciclo para Modificar un Array de 10 Campos con Temporizador Manual

--- P.E.V - TP 1/Assets/Scenes/Scripts/PracticaDos.cs
+++ P.E.V - TP 1/Assets/Scenes/Scripts/PracticaDos.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float timer = 0f;
+    float tiempoLimite = 4.5f;
     void Start()
     {
 
@@ -20,10 +21,18 @@ public class NewBehaviourScript : MonoBehaviour
         Debug.Log(promedio);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
 
+        // Habría preferido ir por == para que se ejecute una sola vez, 
+        // pero eso tal vez suponia redondear tanto el timer como el tiempoLimite así que lo evite porque 
+        // no se si era optimo o si lo vimos
+
+        if (timer >= tiempoLimite)
+        {
+            ModificacionDeArrays();
+        }
     }
 
     public string Promedio(float val1, float val2, float val3)
@@ -49,4 +58,29 @@ public class NewBehaviourScript : MonoBehaviour
         return $"El valor promedio es {promedio}";
     }
 
+    public void ModificacionDeArrays()
+    {
+
+        // Este arreglo podría tener un scope más global, lo dejé acá simplemente para no toparme con él apenas ingreso al código 
+        // así quedaban más separadas las funciones y actividades 
+        // pero si su uso fuese más general a lo largo del script 
+        // (como por ej, valores dinámicos para dar sensación de aleatoriedad o correspondencia) lo pondría más arriba
+
+        int[] arreglo = { 97, -64, -3, -58, -15, 58, 51, 38, -31, -37 };
+
+        // Evite gpt así que sume que imprima por cada elemento del arreglo, me hubiese gustado imprimir el arreglo completo 
+        // pero al hacerlo solo se mostraba el tipo.
+
+        Debug.Log("Arreglo modificado: ");
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            if (arreglo[i] % 2 == 0)
+            {
+                arreglo[i] = arreglo[i] * 2;
+            }
+
+            Debug.Log(arreglo[i]);
+        }
+
+    }
 }
